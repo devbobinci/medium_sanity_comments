a7724238ca99adc408a6e76600b9757bf56c7d32
Bartek Karge <bob-ziomal@MacBook-Pro-Bartek.local>
2023-04-18 20:55:43 +0200

    changed gitignore

--- client.js
+++ client.js
@@ -1,5 +1,4 @@
 import { createClient } from "@sanity/client";
-import { createImageUrlBuilder, createCurrentUserHook } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 // Import using ESM URL imports in environments that supports it:
 // import {createClient} from 'https://esm.sh/@sanity/client'
@@ -8,8 +7,8 @@ const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export const client = createClient({
-  projectId,
-  dataset,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   useCdn: process.env.NODE_ENV === "production", // set to `true` to fetch from edge cache
   apiVersion: "2023-04-09", // use current date (YYYY-MM-DD) to target the latest API version
   token: process.env.SANITY_API_TOKEN, // Only if you want to update content with the client
