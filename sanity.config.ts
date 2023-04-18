6b21064d7ef7e33370833fbdc6a17733a1332644
Bartek Karge <bob-ziomal@MacBook-Pro-Bartek.local>
2023-04-18 21:05:38 +0200

    added basePath into client config

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,7 @@ import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
+  basePath:"/studio",
   name: 'default',
   title: 'medium_w_comments',
 
